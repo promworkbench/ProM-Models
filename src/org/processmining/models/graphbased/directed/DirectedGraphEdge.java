4f62915b4be092c1335c4e841756401d17bd1c30
bfvdonge <b.f.v.dongen@tue.nl>
2016-10-16 05:49:50 +0000

    Added JavaDoc
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/ProM-Models/Trunk@31122 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/models/graphbased/directed/DirectedGraphEdge.java
+++ src/org/processmining/models/graphbased/directed/DirectedGraphEdge.java
@@ -5,10 +5,22 @@ import java.util.Collection;
 public interface DirectedGraphEdge<S extends DirectedGraphNode, T extends DirectedGraphNode> extends
 		DirectedGraphElement {
 
+	/**
+	 * Implementations of this class should also implement DirectedGraphEdge<N, ?>
+	 * @author bfvdonge
+	 *
+	 * @param <N>
+	 */
 	public static interface MultipleSources<N extends DirectedGraphNode> {
 		Collection<N> getSources();
 	}
 
+	/**
+	 * Implementations of this class should also implement DirectedGraphEdge<?, N>
+	 * @author bfvdonge
+	 *
+	 * @param <N>
+	 */
 	public static interface MultipleTargets<N extends DirectedGraphNode> {
 		Collection<N> getTargets();
 	}
