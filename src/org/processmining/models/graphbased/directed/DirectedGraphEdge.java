d7cf4863f9b68ac560a9a1f0b4e60f8e210ea416
bfvdonge <b.f.v.dongen@tue.nl>
2016-10-16 06:10:32 +0000

    Added documentation
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/ProM-Models/Trunk@31123 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/models/graphbased/directed/DirectedGraphEdge.java
+++ src/org/processmining/models/graphbased/directed/DirectedGraphEdge.java
@@ -6,22 +6,39 @@ public interface DirectedGraphEdge<S extends DirectedGraphNode, T extends Direct
 		DirectedGraphElement {
 
 	/**
-	 * Implementations of this class should also implement DirectedGraphEdge<N, ?>
+	 * Implementations of this class should also implement DirectedGraphEdge<N,
+	 * ?>
+	 * 
 	 * @author bfvdonge
-	 *
+	 * 
 	 * @param <N>
 	 */
 	public static interface MultipleSources<N extends DirectedGraphNode> {
+		/**
+		 * returns all target nodes of this DirectedGraphEdge, including the
+		 * node returned by getSource() of DirectedGraphEdge.
+		 * 
+		 * @return
+		 */
 		Collection<N> getSources();
 	}
 
 	/**
-	 * Implementations of this class should also implement DirectedGraphEdge<?, N>
+	 * Implementations of this class should also implement DirectedGraphEdge<?,
+	 * N>
+	 * 
 	 * @author bfvdonge
-	 *
+	 * 
 	 * @param <N>
 	 */
 	public static interface MultipleTargets<N extends DirectedGraphNode> {
+
+		/**
+		 * returns all target nodse of this DirectedGraphEdge, including the
+		 * node returned by getTarget() of DirectedGraphEdge.
+		 * 
+		 * @return
+		 */
 		Collection<N> getTargets();
 	}
 
